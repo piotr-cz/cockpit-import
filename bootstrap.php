ccf5f8ae6789a54125142d8d1bc5e4c874aa1527
Piotr Konieczny <hello@piotr.cz>
2016-08-16 20:54:45 +0200

    update metadata

--- bootstrap.php
+++ bootstrap.php
@@ -5,9 +5,6 @@
 
 // ADMIN
 if (COCKPIT_ADMIN && !COCKPIT_REST) {
-    // Set error reporting (Enable ONLY for debugging)
-    // error_reporting(E_ALL);
-    // ini_set('display_errors', true);
 
     include_once __DIR__ . '/admin.php';
 }
